a917182d4ead63a793d7955586b8aa4218566102
kevinhipeau <kevin.hipeau@gmail.COM>
2017-02-24 09:20:16 +0100

    Exercise 1

--- test/program.test.js
+++ test/program.test.js
@@ -5,8 +5,35 @@ var transformCheckpoint = require('../src/program').transformCheckpoint;
 
 describe('Function transformCheckpoint', function() {
 
-  it('Function transformCheckpoint without parameter should return false', function() {
-    expect(transformCheckpoint()).to.be.false;
-  });
+	it('Function transformCheckpoint without parameter should return false', function() {
+		expect(transformCheckpoint()).to.be.false;
+	});
+	it('Function transformCheckpoint avec parametre doit retourner true et supprimer les parametres', function() {
+		var tmp = {
+			id: 'whataw0nd3rful1d',
+			uuid: 'whataw0nd3rful1d',
+			address: 'unknown',
+			addressType: 'unknown',
+			connectable: true,
+			advertisement: {
+				localName: undefined,
+				txPowerLevel: undefined,
+				manufacturerData: undefined,
+				serviceData: [],
+				serviceUuids: [ 'abcd' ]
+			},
+			rssi: -66,
+			services: null,
+			state: 'outofcontrol'
+		}
+		expect(transformCheckpoint(tmp)).to.be.true;
+		
+		expect(tmp.id).to.equal(undefined);
+		expect(tmp.address).to.equal(undefined);
+		expect(tmp.addressType).to.equal(undefined);
+		expect(tmp.advertisement).to.equal(undefined);
+		expect(tmp.rssi).to.equal(undefined);
+		expect(tmp.services).to.equal(undefined);	
+	});
 
 });
